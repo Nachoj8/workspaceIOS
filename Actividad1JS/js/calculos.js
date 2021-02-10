8d480f4b13dd887097ef4a0ff9849ae7d3ddb521
Nachoj8 <nachojimenezr8@gmail.com>
2021-02-10 14:03:52 +0100

    Actividad1JS

---
+++ Actividad1JS/js/calculos.js
@@ -0,0 +1,21 @@
+function suma(a,b){
+    var resultado = a + b;
+    return resultado;
+  }
+
+function resta(a,b){
+    var resultado = a - b;
+    return resultado;
+}
+  
+  
+function multiplicar(a,b){
+    var resultado = a * b;
+    return resultado;
+}
+   
+
+function dividir(a,b){
+    var resultado = a / b;
+    return resultado;
+}  
